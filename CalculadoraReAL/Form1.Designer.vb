4b25675b57413f663b15bbfde79f7238d112b28e
Jameees <108363737+PowerSuppply@users.noreply.github.com>
2023-07-04 18:01:58 -0600

    V.1

---
+++ CalculadoraReAL/Form1.Designer.vb
@@ -0,0 +1,298 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Boton1 = New Button()
+        Boton2 = New Button()
+        boton3 = New Button()
+        boton4 = New Button()
+        boton5 = New Button()
+        boton6 = New Button()
+        boton7 = New Button()
+        boton8 = New Button()
+        boton9 = New Button()
+        botonSuma = New Button()
+        botonResta = New Button()
+        botonMultiplicacion = New Button()
+        boton0 = New Button()
+        botonIgual = New Button()
+        botonDivision = New Button()
+        pantalla = New Label()
+        Result = New Label()
+        operacion = New Label()
+        ClearButton = New Button()
+        SuspendLayout()
+        ' 
+        ' Boton1
+        ' 
+        Boton1.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        Boton1.Location = New Point(241, 164)
+        Boton1.Name = "Boton1"
+        Boton1.Size = New Size(59, 53)
+        Boton1.TabIndex = 0
+        Boton1.Text = "1"
+        Boton1.TextAlign = ContentAlignment.TopCenter
+        Boton1.UseVisualStyleBackColor = True
+        ' 
+        ' Boton2
+        ' 
+        Boton2.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        Boton2.Location = New Point(326, 164)
+        Boton2.Name = "Boton2"
+        Boton2.Size = New Size(59, 53)
+        Boton2.TabIndex = 1
+        Boton2.Text = "2"
+        Boton2.TextAlign = ContentAlignment.TopCenter
+        Boton2.UseVisualStyleBackColor = True
+        ' 
+        ' boton3
+        ' 
+        boton3.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        boton3.Location = New Point(411, 164)
+        boton3.Name = "boton3"
+        boton3.Size = New Size(59, 53)
+        boton3.TabIndex = 2
+        boton3.Text = "3"
+        boton3.TextAlign = ContentAlignment.TopCenter
+        boton3.UseVisualStyleBackColor = True
+        ' 
+        ' boton4
+        ' 
+        boton4.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        boton4.Location = New Point(241, 241)
+        boton4.Name = "boton4"
+        boton4.Size = New Size(59, 53)
+        boton4.TabIndex = 3
+        boton4.Text = "4"
+        boton4.TextAlign = ContentAlignment.TopCenter
+        boton4.UseVisualStyleBackColor = True
+        ' 
+        ' boton5
+        ' 
+        boton5.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        boton5.Location = New Point(326, 241)
+        boton5.Name = "boton5"
+        boton5.Size = New Size(59, 53)
+        boton5.TabIndex = 4
+        boton5.Text = "5"
+        boton5.TextAlign = ContentAlignment.TopCenter
+        boton5.UseVisualStyleBackColor = True
+        ' 
+        ' boton6
+        ' 
+        boton6.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        boton6.Location = New Point(411, 241)
+        boton6.Name = "boton6"
+        boton6.Size = New Size(59, 53)
+        boton6.TabIndex = 5
+        boton6.Text = "6"
+        boton6.TextAlign = ContentAlignment.TopCenter
+        boton6.UseVisualStyleBackColor = True
+        ' 
+        ' boton7
+        ' 
+        boton7.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        boton7.Location = New Point(241, 322)
+        boton7.Name = "boton7"
+        boton7.Size = New Size(59, 53)
+        boton7.TabIndex = 6
+        boton7.Text = "7"
+        boton7.TextAlign = ContentAlignment.TopCenter
+        boton7.UseVisualStyleBackColor = True
+        ' 
+        ' boton8
+        ' 
+        boton8.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        boton8.Location = New Point(326, 322)
+        boton8.Name = "boton8"
+        boton8.Size = New Size(59, 53)
+        boton8.TabIndex = 7
+        boton8.Text = "8"
+        boton8.TextAlign = ContentAlignment.TopCenter
+        boton8.UseVisualStyleBackColor = True
+        ' 
+        ' boton9
+        ' 
+        boton9.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        boton9.Location = New Point(411, 322)
+        boton9.Name = "boton9"
+        boton9.Size = New Size(59, 53)
+        boton9.TabIndex = 8
+        boton9.Text = "9"
+        boton9.TextAlign = ContentAlignment.TopCenter
+        boton9.UseVisualStyleBackColor = True
+        ' 
+        ' botonSuma
+        ' 
+        botonSuma.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        botonSuma.Location = New Point(493, 164)
+        botonSuma.Name = "botonSuma"
+        botonSuma.Size = New Size(59, 53)
+        botonSuma.TabIndex = 9
+        botonSuma.Text = "+"
+        botonSuma.TextAlign = ContentAlignment.TopCenter
+        botonSuma.UseVisualStyleBackColor = True
+        ' 
+        ' botonResta
+        ' 
+        botonResta.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        botonResta.Location = New Point(493, 241)
+        botonResta.Name = "botonResta"
+        botonResta.Size = New Size(59, 53)
+        botonResta.TabIndex = 10
+        botonResta.Text = "-"
+        botonResta.TextAlign = ContentAlignment.TopCenter
+        botonResta.UseVisualStyleBackColor = True
+        ' 
+        ' botonMultiplicacion
+        ' 
+        botonMultiplicacion.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        botonMultiplicacion.Location = New Point(493, 322)
+        botonMultiplicacion.Name = "botonMultiplicacion"
+        botonMultiplicacion.Size = New Size(59, 53)
+        botonMultiplicacion.TabIndex = 11
+        botonMultiplicacion.Text = "*"
+        botonMultiplicacion.TextAlign = ContentAlignment.TopCenter
+        botonMultiplicacion.UseVisualStyleBackColor = True
+        ' 
+        ' boton0
+        ' 
+        boton0.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        boton0.Location = New Point(241, 405)
+        boton0.Name = "boton0"
+        boton0.Size = New Size(144, 53)
+        boton0.TabIndex = 12
+        boton0.Text = "0"
+        boton0.TextAlign = ContentAlignment.TopCenter
+        boton0.UseVisualStyleBackColor = True
+        ' 
+        ' botonIgual
+        ' 
+        botonIgual.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        botonIgual.Location = New Point(411, 405)
+        botonIgual.Name = "botonIgual"
+        botonIgual.Size = New Size(59, 53)
+        botonIgual.TabIndex = 13
+        botonIgual.Text = "="
+        botonIgual.TextAlign = ContentAlignment.TopCenter
+        botonIgual.UseVisualStyleBackColor = True
+        ' 
+        ' botonDivision
+        ' 
+        botonDivision.Font = New Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point)
+        botonDivision.Location = New Point(493, 405)
+        botonDivision.Name = "botonDivision"
+        botonDivision.Size = New Size(59, 53)
+        botonDivision.TabIndex = 14
+        botonDivision.Text = "/"
+        botonDivision.TextAlign = ContentAlignment.TopCenter
+        botonDivision.UseVisualStyleBackColor = True
+        ' 
+        ' pantalla
+        ' 
+        pantalla.BorderStyle = BorderStyle.FixedSingle
+        pantalla.Font = New Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point)
+        pantalla.Location = New Point(241, 34)
+        pantalla.Name = "pantalla"
+        pantalla.Size = New Size(311, 103)
+        pantalla.TabIndex = 15
+        pantalla.TextAlign = ContentAlignment.TopRight
+        ' 
+        ' Result
+        ' 
+        Result.Font = New Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point)
+        Result.Location = New Point(443, 87)
+        Result.Name = "Result"
+        Result.Size = New Size(96, 39)
+        Result.TabIndex = 16
+        Result.TextAlign = ContentAlignment.TopRight
+        ' 
+        ' operacion
+        ' 
+        operacion.Font = New Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point)
+        operacion.Location = New Point(242, 35)
+        operacion.Name = "operacion"
+        operacion.Size = New Size(298, 53)
+        operacion.TabIndex = 17
+        operacion.TextAlign = ContentAlignment.TopRight
+        ' 
+        ' ClearButton
+        ' 
+        ClearButton.Font = New Font("Segoe UI", 23F, FontStyle.Bold, GraphicsUnit.Point)
+        ClearButton.Location = New Point(161, 164)
+        ClearButton.Name = "ClearButton"
+        ClearButton.Size = New Size(59, 53)
+        ClearButton.TabIndex = 18
+        ClearButton.Text = "C"
+        ClearButton.TextAlign = ContentAlignment.TopCenter
+        ClearButton.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 484)
+        Controls.Add(ClearButton)
+        Controls.Add(operacion)
+        Controls.Add(Result)
+        Controls.Add(pantalla)
+        Controls.Add(botonDivision)
+        Controls.Add(botonIgual)
+        Controls.Add(boton0)
+        Controls.Add(botonMultiplicacion)
+        Controls.Add(botonResta)
+        Controls.Add(botonSuma)
+        Controls.Add(boton9)
+        Controls.Add(boton8)
+        Controls.Add(boton7)
+        Controls.Add(boton6)
+        Controls.Add(boton5)
+        Controls.Add(boton4)
+        Controls.Add(boton3)
+        Controls.Add(Boton2)
+        Controls.Add(Boton1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Boton1 As Button
+    Friend WithEvents Boton2 As Button
+    Friend WithEvents boton3 As Button
+    Friend WithEvents boton4 As Button
+    Friend WithEvents boton5 As Button
+    Friend WithEvents boton6 As Button
+    Friend WithEvents boton7 As Button
+    Friend WithEvents boton8 As Button
+    Friend WithEvents boton9 As Button
+    Friend WithEvents botonSuma As Button
+    Friend WithEvents botonResta As Button
+    Friend WithEvents botonMultiplicacion As Button
+    Friend WithEvents boton0 As Button
+    Friend WithEvents botonIgual As Button
+    Friend WithEvents botonDivision As Button
+    Friend WithEvents pantalla As Label
+    Friend WithEvents Result As Label
+    Friend WithEvents operacion As Label
+    Friend WithEvents ClearButton As Button
+End Class
---
+++ CalculadoraReAL/Form1.vb
@@ -0,0 +1,120 @@
+﻿Imports System.Linq.Expressions
+
+Public Class Form1
+    Public signo, n1, n2 As String
+    Public int1, int2 As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Boton1.Click
+        operacion.Text = operacion.Text & Boton1.Text
+
+    End Sub
+
+    Private Sub operacion_Click(sender As Object, e As EventArgs) Handles operacion.Click
+
+    End Sub
+
+    Private Sub Boton2_Click(sender As Object, e As EventArgs) Handles Boton2.Click
+        operacion.Text = operacion.Text & Boton2.Text
+
+    End Sub
+
+    Private Sub boton3_Click(sender As Object, e As EventArgs) Handles boton3.Click
+        operacion.Text = operacion.Text & boton3.Text
+
+    End Sub
+
+    Private Sub boton4_Click(sender As Object, e As EventArgs) Handles boton4.Click
+        operacion.Text = operacion.Text & boton4.Text
+
+    End Sub
+
+    Private Sub boton5_Click(sender As Object, e As EventArgs) Handles boton5.Click
+        operacion.Text = operacion.Text & boton5.Text
+
+    End Sub
+
+    Private Sub boton6_Click(sender As Object, e As EventArgs) Handles boton6.Click
+        operacion.Text = operacion.Text & boton6.Text
+
+    End Sub
+
+    Private Sub boton7_Click(sender As Object, e As EventArgs) Handles boton7.Click
+        operacion.Text = operacion.Text & boton7.Text
+
+    End Sub
+
+    Private Sub boton8_Click(sender As Object, e As EventArgs) Handles boton8.Click
+        operacion.Text = operacion.Text & boton8.Text
+
+    End Sub
+
+    Private Sub boton9_Click(sender As Object, e As EventArgs) Handles boton9.Click
+        operacion.Text = operacion.Text & boton9.Text
+
+    End Sub
+
+    Private Sub boton0_Click(sender As Object, e As EventArgs) Handles boton0.Click
+        operacion.Text = operacion.Text & boton0.Text
+
+    End Sub
+
+    Private Sub botonSuma_Click(sender As Object, e As EventArgs) Handles botonSuma.Click
+        operacion.Text = operacion.Text & botonSuma.Text
+
+    End Sub
+
+    Private Sub botonResta_Click(sender As Object, e As EventArgs) Handles botonResta.Click
+        operacion.Text = operacion.Text & botonResta.Text
+
+    End Sub
+
+    Private Sub botonMultiplicacion_Click(sender As Object, e As EventArgs) Handles botonMultiplicacion.Click
+        operacion.Text = operacion.Text & botonMultiplicacion.Text
+
+    End Sub
+
+    Private Sub botonDivision_Click(sender As Object, e As EventArgs) Handles botonDivision.Click
+        operacion.Text = operacion.Text & botonDivision.Text
+    End Sub
+
+    Private Sub botonIgual_Click(sender As Object, e As EventArgs) Handles botonIgual.Click
+        For Each i As Char In operacion.Text
+            If Not IsNumeric(i) Then
+                signo = i
+            End If
+            If IsNumeric(i) AndAlso String.IsNullOrEmpty(signo) Then
+                n1 = n1 & i
+            End If
+            If IsNumeric(i) = True AndAlso Not String.IsNullOrEmpty(signo) Then
+                n2 = n2 & i
+            End If
+        Next
+        Select Case signo
+            Case "+"
+                Integer.TryParse(n1, int1)
+                Integer.TryParse(n2, int2)
+                Result.Text = int1 + int2
+            Case "-"
+                Integer.TryParse(n1, int1)
+                Integer.TryParse(n2, int2)
+                Result.Text = int1 - int2
+            Case "*"
+                Integer.TryParse(n1, int1)
+                Integer.TryParse(n2, int2)
+                Result.Text = int1 * int2
+            Case "/"
+                Integer.TryParse(n1, int1)
+                Integer.TryParse(n2, int2)
+                Result.Text = int1 / int2
+        End Select
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        operacion.Text = ""
+        Result.Text = ""
+        int1 = 0
+        int2 = 0
+        n1 = 0
+        n2 = 0
+        signo = ""
+    End Sub
+End Class
